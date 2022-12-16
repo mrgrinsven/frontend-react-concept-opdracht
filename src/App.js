5a69e4b11269c8b60d561fe8774e97983e800c4a
mrgrinsven <mrgrinsven@gmail.com>
2022-12-17 00:26:03 +0100

    Routing

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
+
 import React from 'react';
+import {Routes, Route} from 'react-router-dom';
+
 import './App.css';
+
+import Home from './pages/home/Home';
+import Subreddit from './pages/subreddit/Subreddit'
 
 function App() {
   return (
       <>
+          <Routes>
+              <Route path="/" element={<Home/>}/>
+              <Route path="/subreddit/:subredditId" element={<Subreddit/>}/>
+          </Routes>
 
       </>
   );
---
+++ src/pages/subreddit/Subreddit.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './Subreddit.css';
+
+const Subreddit = () => {
+    return (
+        <>
+            <h1>SUBREDDIT</h1>
+        </>
+    );
+};
+
+export default Subreddit;
